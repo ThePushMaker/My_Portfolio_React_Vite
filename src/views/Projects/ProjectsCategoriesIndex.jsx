89b3cefbab193bcf697f95d053007b5fe06a2842
DSW <elmcwl@gmail.com>
2023-09-13 01:24:07 -0700

    style gradients tab projects and project item

--- src/views/Projects/ProjectsCategoriesIndex.jsx
+++ src/views/Projects/ProjectsCategoriesIndex.jsx
@@ -3,7 +3,7 @@ import { MyContext } from "../../MyContext";
 import ProjectsCategoryCommon from '../../components/Projects/ProjectsCategoryCommon'
 
 const ProjectsCategoriesIndex = () => {
-  const { title, categories, description, URL } = useContext(MyContext);
+  const { title, categories, description0, URL } = useContext(MyContext);
 
   return <ProjectsCategoryCommon title={title} description={description} categories={categories} URL={URL} />;
 };
