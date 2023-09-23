ce4420170cc5a6f2c1ee1096e22b16f7b71c3e73
DSW <elmcwl@gmail.com>
2023-09-23 09:43:09 -0700

    feat: SelectLanguage minified

--- src/components/NavBar/SelectLanguage/SelectLanguage.jsx
+++ src/components/NavBar/SelectLanguage/SelectLanguage.jsx
@@ -5,14 +5,14 @@ import { useEffect, useState } from "react";
 
 const SelectLanguage = () => { 
   const [ t, i18n ] = useTranslation("global"); 
-
-  const [navLng, setNavLng] = useState(null);
+  const { language, updateLanguage } = useMyLanguageContext();
+  const [navLng, setNavLng] = useState('en');
   
   const handleLanguageChange = (e) => {
     const selectLanguage = e.target.value;
     i18n.changeLanguage(selectLanguage);
     setNavLng(selectLanguage); 
-    updateLanguage(selectLanguage);
+    // updateLanguage(selectLanguage);
   }
 
   
@@ -24,21 +24,10 @@ const SelectLanguage = () => {
     setNavLng(navLng);
     i18n.changeLanguage(navLng);
     
-  }, []); 
-  
-  console.log(i18n.language)
-  
-  
-  
-  const { language, updateLanguage } = useMyLanguageContext();
-  console.log("aaa")
-  console.log(language)
-  // aqui puedes usar data y updateData para acceder y actualizas los datos del contexto
-
+  }, []);  
 
   return(
     <>
-      Browser Language {navLng}
       <div className="py-[4px] px-2 xl:px-5">
         <select 
           className={`navbar-link text-[17px] xl:text-xl active bg-transparent w-24`} 
