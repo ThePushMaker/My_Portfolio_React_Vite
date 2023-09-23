ed0ad134ae98e6d6c060858985957ed6c83ee88b
DSW <elmcwl@gmail.com>
2023-09-23 09:24:52 -0700

    feat: autochange language to default navigator language

--- src/components/NavBar/SelectLanguage/SelectLanguage.jsx
+++ src/components/NavBar/SelectLanguage/SelectLanguage.jsx
@@ -1,38 +1,55 @@
 import { useTranslation } from "react-i18next";
 import { LANGUAGES } from "../../../constants/constants";
 import {useMyLanguageContext} from '../../../contexts/LanguageContext'
+import { useEffect, useState } from "react";
 
 const SelectLanguage = () => { 
   const [ t, i18n ] = useTranslation("global"); 
 
+  const [navLng, setNavLng] = useState(null);
   
-  const languageCode = navigator.language;
-  // Utilizamos el método split para dividir la cadena en función del guión '-'
-  const parts = languageCode.split('-');
-  // La parte que queremos está en la primera posición del array resultante
-  const navLng = parts[0]; // Esto contendrá 'es'
+  const handleLanguageChange = (e) => {
+    const selectLanguage = e.target.value;
+    i18n.changeLanguage(selectLanguage);
+    setNavLng(selectLanguage); 
+    updateLanguage(selectLanguage);
+  }
+
+  
+  useEffect(() => {
+    const languageCode = navigator.language;
+    const parts = languageCode.split('-');
+    const navLng = parts[0];
+
+    setNavLng(navLng);
+    i18n.changeLanguage(navLng);
+    
+  }, []); 
   
   console.log(i18n.language)
+  
+  
   
   const { language, updateLanguage } = useMyLanguageContext();
   console.log("aaa")
   console.log(language)
   // aqui puedes usar data y updateData para acceder y actualizas los datos del contexto
 
+
   return(
     <>
-      {/* Browser Language {navLng} */}
+      Browser Language {navLng}
       <div className="py-[4px] px-2 xl:px-5">
         <select 
           className={`navbar-link text-[17px] xl:text-xl active bg-transparent w-24`} 
-          defaultValue={navLng} 
+          value={navLng} 
+          onChange={handleLanguageChange}
         >
         {
           LANGUAGES.map(({ code, label }) => (
             <option className="bg-purple"
               key={code}
               value={code}
-              onClick={() => i18n.changeLanguage(code)} 
             >
               {t(`navbar.${label}`)}
             </option>
