c58c73b364e739fbc39ffa380d316ed53935e70a
DSW <elmcwl@gmail.com>
2023-09-29 01:19:20 -0700

    syle: fix subscribe button newsletter component

--- src/components/MailchimpForm/Newsletter/Newsletter.jsx
+++ src/components/MailchimpForm/Newsletter/Newsletter.jsx
@@ -40,7 +40,7 @@ const clearFields = () => {
       <div className="newsletter-bx
       py-[20px]
       md:py-[50px] px-10  
-      lg:py-[85px] lg:px-[125px]">
+      lg:py-[85px] lg:px-24 xl:px-[125px]">
         <div className="flex flex-wrap items-center w-full">
           <div className="w-full lg:w-5/12 py-3">
             <p className="text-[1.2rem] md:text-[1.35rem] lg:mr-4 text-[black] font-bold text-center"> {t(`newsletter.main_message`)} </p>
@@ -49,7 +49,7 @@ const clearFields = () => {
             <form onSubmit={handleSubmit}>
                <div className="flex flex-wrap justify-end">
                 <div className="new-email-bx w-full mb-3 md:m-0 
-                  md:w-8/12"
+                  md:w-7/12"
                 >
                   <input 
                     className="focus:outline-border_color1 focus:ring-border_color1 w-full"
@@ -59,11 +59,11 @@ const clearFields = () => {
                     placeholder= {t(`newsletter.placeholder_email`)}
                     />
                 </div>
-                <div className=" w-52 m-auto 
-                  md:w-4/12 md:m-0 md:pl-3 "
+                <div className="w-fit m-auto 
+                   md:m-0 md:pl-3 "
                 >
-                  <button className="newsletter_button w-full bg-[black] bg-opacity-[88%]" type="submit">
-                    <span>
+                  <button className="newsletter_button  bg-[black] bg-opacity-[88%] w-fit" type="submit">
+                    <span className="">
                       {t(`newsletter.submit`)}
                     </span>
                   </button>
