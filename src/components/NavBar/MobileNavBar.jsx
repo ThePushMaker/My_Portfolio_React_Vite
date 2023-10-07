21ec772cfc4bd484a6c22fadf9ddfc69acfa739f
DSW <elmcwl@gmail.com>
2023-10-07 14:04:24 -0700

    fix: selectlanguage navbar

--- src/components/NavBar/MobileNavBar.jsx
+++ src/components/NavBar/MobileNavBar.jsx
@@ -8,7 +8,7 @@ const MobileNavBar = ({ NavbarRoutesHome, NavbarRoutes }) => {
   const [openMobileNavbar, setOpenMobileNavbar] = useState(false);
   const navbarRef = useRef(null);
   const buttonRef = useRef(null);
-
+  
   // Manejador de clics fuera de la navbar
   useEffect(() => {
     function handleClickOutside(event) {
@@ -72,8 +72,8 @@ const MobileNavBar = ({ NavbarRoutesHome, NavbarRoutes }) => {
         
         {/* hamburguer button */}
 
-        <button className="h-full flex justify-center items-center cursor-pointer z-20" type="button" ref={buttonRef}>
-          <div className={`visible inline lg:invisible lg:hidden svg_link1`} >
+        <button className="h-full flex justify-center items-center cursor-pointer" type="button" ref={buttonRef}>
+          <div className={`visible inline lg:invisible lg:hidden svg_link1 z-10`} >
             {!openMobileNavbar ? (
               // hamburguer
               <svg className="h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 6H20M4 12H14M4 18H9" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
