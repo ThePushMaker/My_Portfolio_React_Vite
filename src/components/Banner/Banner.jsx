b30ccab5e35b2a2868496c57bf5ff5cbe3fc001b
DSW <elmcwl@gmail.com>
2023-10-06 20:54:42 -0700

    syle: text size socialIcons component

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -64,7 +64,7 @@ const Banner = () => {
                 <div className="flex ">
                   <div className="pr-16">
                     <a href="#contact">
-                      <button className="mt-6">
+                      <button className="mt-5">
                         <label className={linkStyles}>
                           {t(`banner.connect`)}
                         </label>
@@ -74,7 +74,7 @@ const Banner = () => {
                   </div>
                   <div>
                     <Link className="">
-                      <button className="mt-6">
+                      <button className="mt-5">
                         <label className={linkStyles}>More About Me</label>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="25" height="25" fill="currentColor"><path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"></path></svg>
                       </button>
@@ -82,7 +82,7 @@ const Banner = () => {
                   </div>
         
               </div>
-              <div className="my-auto my-10">
+              <div className="mt-7">
                   <SocialIcons />
               </div>
             </div>
--- src/components/SocialIcons/SocialIcons.jsx
+++ src/components/SocialIcons/SocialIcons.jsx
@@ -1,5 +1,4 @@
 import { Tooltip } from 'react-tooltip';
-
 import navIcon1 from '../../assets/img/icons/nav-icon1.svg';
 import navIcon2 from '../../assets/img/icons/nav-icon2.svg';
 import navIcon3 from '../../assets/img/icons/nav-icon3.svg';
@@ -10,47 +9,48 @@ import './SocialIcons.css'
 const stylesLink = 'w-9 h-9 xl:w-11 xl:h-11 mx-1.5 lg:mx-[3px] xl:mx-[3px]';
 
 const SocialIcons = () => {
-  const [ t ] = useTranslation("global");  
+  const [ t ] = useTranslation("global"); 
   
   const imagesArray = [
     {
       id: 1,
       src: navIcon1,
-      dataTooltip: 'LinkedIn',
-      href: 'https://www.linkedin.com/in/martin-calderon-dev'
+      href: 'https://www.linkedin.com/in/martin-calderon-dev',
+      label: 'LinkedIn'
     },
     {
       id: 2,
       src: navIcon2,
-      dataTooltip: 'GitHub',
-      href: 'https://github.com/DSW-47'
+      href: 'https://github.com/DSW-47',
+      label: 'GitHub'
     },
     {
       id: 3,
       src: navIcon3,
-      dataTooltip: t(`navbar.YouTube Channel`),
-      href: 'https://www.youtube.com/channel/UCNy0OJqXOCyhThyXTcLLCJA?sub_confirmation=1'
+      dataTooltip: 'Programación para Todos',
+      href: 'https://www.youtube.com/channel/UCNy0OJqXOCyhThyXTcLLCJA?sub_confirmation=1',
+      label: t(`navbar.YouTube`)
     },
     {
       id: 4,
       src: navIcon4,
       dataTooltip: t(`navbar.WakaTime Stats`),
-      href: 'https://wakatime.com/@dsw47'
+      href: 'https://wakatime.com/@dsw47',
+      label: 'WakaTime'
     },
   ];
   
   return(
     <>
       <div className='social-icon'>
-      {imagesArray.map((item, index) => (
-        <a key={index} className={stylesLink} data-tooltip-id='myTooltip' data-tooltip-content={item.dataTooltip}  href={item.href} target='_blank' rel='noreferrer' >
-          <img src={item.src} />
-          <label>LinkedIn</label>
-        </a>
-      ))}
-      <Tooltip id={'myTooltip'} place='top' type='dark' effect='solid' style={{fontSize:'17px'}}/>
+        {imagesArray.map((item, index) => (
+          <a key={index} className={stylesLink} href={item.href} target='_blank' rel='noreferrer' data-tooltip-id='myTooltip' data-tooltip-content={item.dataTooltip}>
+            <img src={item.src} />
+            <label>{item.label}</label>
+          </a>
+        ))}
+        <Tooltip id={'myTooltip'} place='top' type='dark' effect='solid'  style={{fontSize:'17px'}}/>
       </div>
-      
     </>
   );
 }
