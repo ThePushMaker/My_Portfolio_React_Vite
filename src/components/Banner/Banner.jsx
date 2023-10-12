43cc3c385539740ace167db5b5d9fa6cd396fdca
DSW <elmcwl@gmail.com>
2023-10-12 12:37:24 -0700

    style: home responsive corrections

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -76,21 +76,25 @@ const Banner = () => {
               </div>
   
                 <div className="flex mb-6 ">
-                  <div className="pr-16">
+                  <div className="w-6/12">
                     <a href="#contact">
                       <button>
                         <label className={linkStyles}>
                           {t(`banner.connect`)}
                         </label>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="25" height="25" fill="currentColor"><path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"></path></svg>
+                        <div className="justify-start">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="25" height="25" fill="currentColor"><path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"></path></svg>
+                        </div>
                       </button>
                     </a>
                   </div>
-                  <div>
+                  <div className="w-6/12">
                     <Link>
                       <button>
-                        <label className={linkStyles}>{t(`about_me.more_about_me`)}</label>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="25" height="25" fill="currentColor"><path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"></path></svg>
+                        <label className={`${linkStyles} text-right`}>{t(`about_me.more_about_me`)}</label>
+                        <div className="my-0 flex self-start">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="25" height="25" fill="currentColor"><path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"></path></svg>
+                        </div>
                       </button>
                     </Link>
                   </div>
