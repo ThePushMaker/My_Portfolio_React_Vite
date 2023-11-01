92db740fb87de1201e9335d1f8f2a41869bee246
DSW <elmcwl@gmail.com>
2023-10-31 23:39:25 -0700

    feat: added translations for NotFound component

--- src/views/NotFound/NotFound.jsx
+++ src/views/NotFound/NotFound.jsx
@@ -1,22 +1,22 @@
 import { Link } from 'react-router-dom';
 import './NotFound.css'
 import NotFoundTranparentImg from '../../assets/img/404/404_transparent.png'
+import { useTranslation } from 'react-i18next';
 
 const NotFound = () => {
-
-  const linkStyles = 'text-link_color hover:text-link_color_hover hover:underline textShadow cursor-pointer'
-
+  const [ t ] = useTranslation("global");  
+  
   return(
     <>
       <div className="simple_background flex  h-screen justify-center items-center text-[white]">
         <div>
           <h2 className='text-link_disabled_color'>
-            Not Found
+            {t('default.not_found_title')}
           </h2>
    
           <img src={NotFoundTranparentImg} alt='Not_Found_Image'  className="w-[450px] mx-auto" />
      
-          <p className='text-center text-[#ddd]'>This page cannot be found</p>
+          <p className='text-center text-[#ddd]'>{t('default.not_found_message')}{'.'}</p>
   
           <Link
           to="/"
@@ -26,9 +26,8 @@ const NotFound = () => {
               flex m-auto justify-center mt-6 w-fit border-[1.5px] border-solid border-border_color1 bg-[black] bg-opacity-40" 
             >
               <span>
-                Back to homepage
-                {/* {t('TabPanelItem.display_more')} */}
-                {/* {mostrarMas ? "Display Less" : "Display More"} */}
+                
+                {t('default.not_found_button')}
               </span>
             </button>
           </Link>
