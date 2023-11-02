f3b92c07d4dd6ecdeba3b911d5dcd929cd6c099b
DSW <elmcwl@gmail.com>
2023-11-01 18:57:21 -0700

    feat: added not found view to the ProjectDetail component in case of not existing url

--- src/views/Projects/ProjectDetail.jsx
+++ src/views/Projects/ProjectDetail.jsx
@@ -2,11 +2,12 @@ import { useContext } from "react";
 import { ArticleProjectsContext } from "../../contexts/ArticleProjectsContext";
 import ProjectsCategoryCommon from "../../components/Projects/ProjectsCategoryCommon";
 
-import { useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 import { getNameByURL, getParentURLByCurrentURL } from '../../routes';
 import { useTranslation } from "react-i18next";
 import projectsJSON_en from "../../data/en/projects.json";
 import projectsJSON_es from "../../data/es/projects.json";
+import NotFound from "../NotFound/NotFound";
 
 const ProjectDetail= () => {
   const [ t] = useTranslation("global"); 
@@ -19,13 +20,12 @@ const ProjectDetail= () => {
   const categoryName = getNameByURL(getParentURLByCurrentURL(URL),storedLanguage);
   
   let categories;
-  
-  if(storedLanguage === 'en') {
-    categories=projectsJSON_en.categories
-  }
-  else if(storedLanguage === 'es') {
+  if(storedLanguage === 'es') {
     categories=projectsJSON_es.categories
   } 
+  else {
+    categories=projectsJSON_en.categories
+  }
 
   let categoryData = null;
   let projectsInfo = null;
@@ -43,12 +43,16 @@ const ProjectDetail= () => {
 
   return(
     <>
+      {projectsInfo ? (
        <ProjectsCategoryCommon 
             projectCategory={categoryData.category} 
             projectInfo={projectsInfo} 
             title={t(`projectDetail.title`)+`: ${projectsInfo.title}`} 
             description={idProject}  
         />
+        ) : (
+          <NotFound />
+        )}
     </>
   );
 };
