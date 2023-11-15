af83b64d94d98cd7f9188b12bee849a045c1ec2c
DSW <elmcwl@gmail.com>
2023-11-15 00:59:34 -0700

    style: add better style to ProjectItemCard

--- src/components/Projects/ProjectItemCard/ProjectItemCard.jsx
+++ src/components/Projects/ProjectItemCard/ProjectItemCard.jsx
@@ -5,19 +5,19 @@ import ProjectImage from "../ProjectImage/ProjectImage"
 const ProjectItemCard = ( {title, description, imgPath} ) => {
  
   return (
-    <motion.div animate={{scale: [0, 1]}} transition={{duration: 0.5}} className="projectItemCard">
-        <div className="proj-imgbx border-[1px] border-border_color2 flex hover:shadow-md hover:shadow-neon_color2 
-          h-[270px]" 
-        >
+    <motion.div animate={{scale: [0, 1]}} transition={{duration: 0.5}} className="projectItemCard border-[1px] border-border_color2 ">
+        <div className="proj-imgbx h-[17rem]">
           <ProjectImage 
             imgPath = { imgPath }
             title = { title }
           />
-          
         </div>
         <div className="textContainer">
           <h4>{title}</h4>
           <span>{description}</span>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias laudantium voluptate, in cumque assumenda perferendis aspernatur officia maxime tempore expedita, commodi vero nisi? Nam vero exercitationem fugit dicta voluptate inventore.
+          </p>
         </div>
     </motion.div>
       
