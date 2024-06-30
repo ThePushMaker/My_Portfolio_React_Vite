cb6dc87aa90622ec1dbdd478bada31cb6143df2a
Martin Calderón <elmcwl@gmail.com>
2024-06-29 23:25:00 -0700

    feat: add translation to social icons tooltips

--- src/components/SocialIcons/SocialIcons.jsx
+++ src/components/SocialIcons/SocialIcons.jsx
@@ -2,7 +2,6 @@ import { Tooltip } from 'react-tooltip';
 import navIcon1 from '../../assets/img/icons/nav-icon1.svg';
 import navIcon2 from '../../assets/img/icons/nav-icon2.svg';
 import navIcon3 from '../../assets/img/icons/nav-icon3.svg';
-import navIcon4 from '../../assets/img/icons/nav-icon4.svg';
 import { useTranslation } from 'react-i18next';
 import './SocialIcons.css'
 
@@ -15,19 +14,21 @@ const SocialIcons = () => {
     {
       id: 1,
       src: navIcon2,
-      href: 'https://github.com/MSCO-2000',
+      dataTooltip: t(`navbar.go_to`)+' GitHub',
+      href: 'https://github.com/ThePushMaker',
       label: 'GitHub'
     },
     {
       id: 2,
       src: navIcon1,
+      dataTooltip: t(`navbar.go_to`)+' LinkedIn',
       href: 'https://www.linkedin.com/in/martin-calderon-dev',
       label: 'LinkedIn'
     },
     {
       id: 3,
       src: navIcon3,
-      dataTooltip: 'Programación para Todos',
+      dataTooltip: t(`navbar.go_to`)+' YouTube',
       href: 'https://www.youtube.com/channel/UCNy0OJqXOCyhThyXTcLLCJA?sub_confirmation=1',
       label: t(`navbar.YouTube`)
     },
