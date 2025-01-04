bf4da745e4c969f83456c16853edf1e730e2a359
Martin Calderón <elmcwl@gmail.com>
2025-01-03 23:03:57 -0700

    refactor: absolute routes to Home.jsx

--- src/views/Pages/Home.jsx
+++ src/views/Pages/Home.jsx
@@ -1,10 +1,10 @@
-import NavBar from '../../components/NavBar/NavBar';
-import Banner from '../../components/Banner/Banner';
-import Skills from '../../components/Skills/Skills';
-import Projects from '../../components/Projects/Projects';
-import Contact from '../../components/Contact/Contact';
-import Footer from '../../components/Footer/Footer';
-import AboutMe from '../AboutMe/AboutMe';
+import NavBar from '@/components/NavBar/NavBar';
+import Banner from '@/components/Banner/Banner';
+import Skills from '@/components/Skills/Skills';
+import Projects from '@/components/Projects/Projects';
+import Contact from '@/components/Contact/Contact';
+import Footer from '@/components/Footer/Footer';
+import AboutMe from '@/views/AboutMe/AboutMe';
 
 function Home() {
   
