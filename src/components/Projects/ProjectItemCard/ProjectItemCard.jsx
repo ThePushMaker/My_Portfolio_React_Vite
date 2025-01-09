c46d9566fd2136fa28002f187ecd320d55061ec8
Martin Calderón <elmcwl@gmail.com>
2025-01-09 00:59:39 -0700

    feat: display technologies icons in ProjectItemCard.jsx

--- src/components/Projects/ProjectItemCard/ProjectItemCard.jsx
+++ src/components/Projects/ProjectItemCard/ProjectItemCard.jsx
@@ -1,10 +1,11 @@
 import { motion } from "framer-motion"
-import ProjectImage from "@/components/Projects/ProjectImage/ProjectImage"
 
+import ProjectImage from "@/components/Projects/ProjectImage/ProjectImage"
+import imgSkillsJson from '@/data/skills/skills.json';
 import "@/components/Projects/ProjectItemCard/ProjectItemCard.css"
 
 const ProjectItemCard = ( {title, main_technologies, disciplines_covered, description, imgPath} ) => {
-  const TechnologiesArray = main_technologies ?Object.values(main_technologies) : [];
+  const mainTechnologies = main_technologies;
   
   return (
     <motion.div animate={{scale: [0, 1]}} transition={{duration: 0.5}} className="projectItemCard border-[1px] border-border_color2 ">
@@ -19,9 +20,33 @@ const ProjectItemCard = ( {title, main_technologies, disciplines_covered, descri
           <h4>{title}</h4>
           <h5>Tecnologías</h5>
           
-          {TechnologiesArray.map((technology, index) => (
-            <span key={index}>{technology}</span>
-          ))}
+          {/* {main_technologies.map((technology, index) => {
+        // Busca el objeto correspondiente en el JSON
+        const tech = technologyData.find(item => item.label === technology);
+        
+        return (
+          <div key={index}>
+            <img src={tech ? tech.img : ''} alt={technology} style={{ width: '30px', height: '30px' }} />
+            {technology}
+          </div>
+        );
+      })} */}
+          
+
+          {mainTechnologies && main_technologies.map((technology, index) => {
+            <div key={index}>
+              {technology}
+            </div>
+          })}
+          
+          {/* {imgSkillsJson.map((item, index) => (
+            <div className='item w-14 h-14 mx-10' key={index}>
+              <img src={item.img} alt={item.label} />
+              <h5>
+                  {item.label}
+              </h5>
+            </div>
+          ))} */}
           
           <span>
             {disciplines_covered}
