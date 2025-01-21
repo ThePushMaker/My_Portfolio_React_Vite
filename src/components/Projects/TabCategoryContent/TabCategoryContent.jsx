f57fc73bdc2af2b76cd5c63992746c20455204c0
Martin Calderón <elmcwl@gmail.com>
2025-01-20 17:50:49 -0700

    style: cleaned TabCategoryContent

--- src/components/Projects/TabCategoryContent/TabCategoryContent.jsx
+++ src/components/Projects/TabCategoryContent/TabCategoryContent.jsx
@@ -23,51 +23,49 @@ const TabCategoryContent = (props) => {
     
     const newURL = routes[props.category];
 
-    
     navigate(newURL); // Perform navigation immediately
-
   };
   
 
   return (
-  <>
-    { (
-      <motion.div animate={props.activeTab === props.index+1 ? "open" : "closed"}
-        variants={variants} transition={{ duration: 0.8 }} 
-        id={`panel-${props.index+1}`}
-        className={`${props.activeTab === props.index+1 ? '' : 'hidden'} `}
-      >
-        <h3 className="pb-[15px] lg:pb-[20px] text-highlighted_text_color">{props.category}</h3>
-        <p className="text-center m-auto pb-[25px] w-[85%] md:w-[90%] lg:pb-[35px] lg:w-[70%]">
-          {props.description}{'.'}
-        </p>
-        
-        <ProjectsListGrid 
-          projects={mostrarRegistros} 
-          category={props.category} 
-        />
-        
-        <div>
-          {props.projects.length > 6 && (
-
-            // { props.category }
-            <button  
-              className="button_transparent_rounded shadow-sm shadow-border_color1 hover:shadow-md hover:shadow-neon_color2
-              flex m-auto justify-center mt-6 w-fit border-[1.5px] border-solid border-border_color1 bg-[black] bg-opacity-40" 
-              onClick={toggleMostrarMas}
-            >
-              <span>
-                {t('TabPanelItem.display_more')}
-                {/* {mostrarMas ? "Display Less" : "Display More"} */}
-              </span>
-            </button>
-      
-          )}
-        </div>
-      </motion.div>
-    )}
+    <>
     
-  </>
+      { (
+        <motion.div
+          animate={props.activeTab === props.index+1 ? "open" : "closed"}
+          variants={variants} transition={{ duration: 0.8 }}
+          id={`panel-${props.index+1}`}
+          className={`${props.activeTab === props.index+1 ? '' : 'hidden'} `}
+        >
+          <h3 className="pb-[15px] lg:pb-[20px] text-highlighted_text_color">{props.category}</h3>
+          <p className="text-center m-auto pb-[25px] w-[85%] md:w-[90%] lg:pb-[35px] lg:w-[70%]">
+            {props.description}{'.'}
+          </p>
+          
+          <ProjectsListGrid 
+            projects={mostrarRegistros}
+            category={props.category}
+          />
+          
+          {/* Botón mostrar más */}
+          <div>
+            {props.projects && props.projects.length > 6 && (
+              <button  
+                className="button_transparent_rounded shadow-sm shadow-border_color1 hover:shadow-md hover:shadow-neon_color2 flex m-auto
+                  justify-center mt-6 w-fit border-[1.5px] border-solid border-border_color1 bg-[black] bg-opacity-40"
+                onClick={toggleMostrarMas}
+              >
+                <span>
+                  {t('TabPanelItem.display_more')}
+                </span>
+              </button>
+            )}
+            
+          </div>
+        </motion.div>
+      )}
+      
+    </>
   );
 };
 
