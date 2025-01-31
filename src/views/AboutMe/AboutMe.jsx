918d97382de3e435207eef23f393957eca451ea2
Martin Calderón <elmcwl@gmail.com>
2025-01-31 12:31:36 -0700

    feat: use absolute routes and update  AboutMe section image on home page

--- src/views/AboutMe/AboutMe.jsx
+++ src/views/AboutMe/AboutMe.jsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
-import CustomHeader from '../../components/UI/CustomHeader/CustomHeader'
+
+import CustomHeader from '@/components/UI/CustomHeader/CustomHeader';
 
 const AboutMe = () => {
   const [t] = useTranslation("global");
@@ -8,32 +9,32 @@ const AboutMe = () => {
   const paragraphStyles = 'mb-2 textShadow text-[#ccc]'
 
   return (
-    <section className="text-center md:text-start mt-0" id="aboutMe">
-      <div className="bg_semitransparent px-6 md:px-14 lg:px-24 pt-0 pb-16 mx-auto">
-
-        <hr className="hr5" />
+    <section className="bg_semitransparent text-center md:text-start mt-0" id="aboutMe">
+      
+      {/* separador */}
+      <div className="px-6 md:px-14 lg:px-24">
+        <hr className="hr5"/>
+      </div>
         
-        {/* left */}
-        <div className="flex flex-wrap-reverse pt-14">
-          <div className="w-full lg:w-6/12 lg:px-8 my-auto flex justify-center">
-            <div className="animatedImg w-96 md:w-80 lg:w-[550px]">
-              <p className={`${paragraphStyles} text-[30px] font-bold text-center`}>{t(`about_me.img_description`)}</p>
+      {/* main content */}
+      <div className="max-w-screen-xl pt-0 pb-16 mx-auto">
 
-              <img 
-                className=" rounded-[3rem] opacity-[92%] mt-5 border-[1.5px] border-border_color1 shadow-[0_2px_12px] shadow-dark_link_color"
-                src={'/src/assets/img/about_me.gif'}
-                alt="Header Img2"
-              />
-              {/* <img className=" rounded-md opacity-[92%] shadow-2xl mb-4"
+        {/* left side */}
+        <div className="flex flex-wrap-reverse pt-14">
+          <div className="w-full lg:w-5/12 lg:px-8 my-auto flex justify-center">
+            <div className="animatedImg w-96 md:w-80 lg:w-80">
+              {/* text of img */}
+              <img className="rounded-[3rem] opacity-[82%] border-[0.124rem] border-border_color1"
                 src={'https://camo.githubusercontent.com/bc425a5c4e99161dca81a32820bad4d14290d90de6308f16eb8e1d904526f5ab/68747470733a2f2f6d656469612e6c6963646e2e636f6d2f646d732f696d6167652f44353631324151474f6d77664945356d6c57412f61727469636c652d636f7665725f696d6167652d736872696e6b5f3732305f313238302f302f313637343631373934373232383f653d3231343734383336343726763d6265746126743d4654555f697351365659665635445f7565464850577654385a716744654a47337972384d69386c70666b30'} 
                 alt="Header Img2"
-              /> */}
+              />
+              <p className={`mt-4 text-3xl font-bold text-center ${paragraphStyles}`}>{t(`about_me.img_description`)}</p>
             </div>
 
           </div>
 
-          {/* right */}
-          <div className="w-full lg:w-6/12 text-center lg:text-left">
+          {/* right side */}
+          <div className="w-full lg:w-7/12 text-center lg:text-left">
             <CustomHeader
               align='left'
               emoji='🚀'
